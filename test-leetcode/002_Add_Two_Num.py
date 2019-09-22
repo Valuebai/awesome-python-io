7c8bd41750b8aeb6fd2f345a913416bf11ff8fc7
luhuibo <luhuibo306@foxmail.com>
2019-09-23 00:11:19 +0800

    【新增】leetcode-02 两个数相加

---
+++ test-leetcode/002_Add_Two_Num.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@IDE    ：PyCharm
+@Author ：LuckyHuibo
+@Date   ：2019/9/22 23:21
+@Desc   ：leetcode-02 2个数相加
+
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+=================================================='''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# 生成list链表
+def generateList(l: list) -> ListNode:
+    prenode = ListNode(0)
+    lastnode = prenode
+    for val in l:
+        lastnode.next = ListNode(val)
+        lastnode = lastnode.next
+    return prenode.next
+
+
+# 打印list链表
+def printList(l: ListNode):
+    while l:
+        print("%d, " % (l.val), end='')
+        l = l.next
+    print('')
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        re = ListNode(0)
+        r = re
+        carry = 0
+        while (l1 or l2):
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            s = carry + x + y
+            carry = s // 10
+            r.next = ListNode(s % 10)
+            r = r.next
+            if (l1 != None): l1 = l1.next
+            if (l2 != None): l2 = l2.next
+        if (carry > 0):
+            r.next = ListNode(1)
+        return re.next
+
+
+if __name__ == "__main__":
+    l1 = generateList([2, 4, 3])
+    l2 = generateList([5, 6, 4])
+    printList(l1)
+    printList(l2)
+    s = Solution()
+    sum =s.addTwoNumbers(l1, l2)
+    printList(sum)
